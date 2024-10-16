0e43d3e915e6c619a0cabb4e269bdc20db660833
Olli Koski <olli.koski@student.careeria.fi>
2024-10-16 20:17:23 +0300

    Alkutilanne

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+  
+
+  return (
+    <>
+      <div>
+       <h1>Opiskelijan Kuvapankki</h1> 
+      </div>
+      
+    </>
+  )
+}
+
+export default App
