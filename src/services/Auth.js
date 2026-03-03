2b14af75db578252e0fdf51f88c6a8777f1507b6
OlliKoskCareeria <124531056+OlliKoskCareeria@users.noreply.github.com>
2026-03-03 20:35:40 +0200

    Deleted commented code

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const baseUrl = "https://localhost:7079/api/Autentikaatio"
-//const baseUrl = "https://northwindrestapi20240529210032.azurewebsites.net/api/authentication"
+
 
 const authenticate = (userForAuth) => {
     const request = axios.post(baseUrl, userForAuth)
